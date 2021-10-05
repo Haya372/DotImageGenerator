c2dfc479f40006c8e1af4df954da0dcab7a8878b
Hayato Mitomi <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-10-05 11:49:47 +0900

    [A] file upload component

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-file-input
+      accept="image/*"
+      label="File input"
+      v-model="image"
+      full-width
+    />
+    <div v-if="image != null">
+      <v-img :src="imageUrl" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FileUpload",
+  data() {
+    return {
+      image: null,
+    };
+  },
+  methods: {
+    onChangeFile(file) {
+      if(file){
+        this.image = URL.createObjectURL(file);
+      }else{
+        this.image = null;
+      }
+    },
+  },
+  computed: {
+    imageUrl(){
+      if(this.image){
+        return URL.createObjectURL(this.image);
+      }
+      return null;
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>Home</div>
+  <div class="home-wrapper">
+    <div>Home</div>
+    <FileUpload />
+  </div>
 </template>
 
 <script>
+import FileUpload from '../components/FileUpload';
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: { FileUpload },
 }
 </script>
+
+<style scoped>
+.home-wrapper {
+  min-width: 640px;
+}
+</style>
