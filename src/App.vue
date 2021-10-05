d4faea98cd27ed8d07cc03da63686993a4bcf89a
Hayato Mitomi <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-10-05 11:20:51 +0900

    [A] Vue and webpack settings

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <div class="wrapper">
+      <router-view />
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+}
+</script>
+
+<style scoped>
+.wrapper {
+  margin: 16px auto;
+  flex-grow: 1;
+  text-align: center;
+  max-width: 820px;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const env = process.env.NODE_ENV || 'development';
+
+const plugins = [
+  new VueLoaderPlugin(),
+  new webpack.DefinePlugin({
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false
+  }),
+  new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+  }),
+  new MiniCssExtractPlugin({
+    filename: "styles.css",
+    chunkFilename: "[name]-[chunkhash].css"
+  }),
+  /*new CopyPlugin({
+    patterns: [
+      {
+        from: "*",
+        to: path.resolve(__dirname, 'dist', 'assets'),
+        context: "src/assets"
+      },
+    ],
+  })*/
+];
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name]-[chunkhash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
+  },
+  devtool: 'source-map',
+  mode: env,
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          'style-loader',
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            /* // Requires >= sass-loader@^8.0.0
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                indentedSyntax: true // optional
+              },
+            },*/
+          },
+        ],
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+  resolve: {
+    extensions: [".js", ".vue"],
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+    },
+  },
+  plugins: plugins
+}
